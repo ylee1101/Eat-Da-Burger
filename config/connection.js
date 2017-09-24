9b84148423afcb37281cbfc937f4daa6e848cf4e
ylee1101 <yongho19911101@hotmail.com>
2017-09-23 17:50:22 -0700

    added info for herku

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
 	connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
+    host: 'xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com    ',
+    user: 'b4ttvezobey98g3h',
+    password: 'jnmmgxjh2f7f7s4m',
     database: 'burgers_db'
   });
 };
